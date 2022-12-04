1392a013880515b6a93fef92858ff66233731894
FelipeSouza-178 <62517496+FelipeSouza-178@users.noreply.github.com>
2022-12-04 20:46:39 +0000

    Modificado certificado

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
 import 'minireset.css';
 import './App.css';
+import logo from './assets/logo-ola-mundos.png';
+import signature from './assets/signature.png';
 
-function App({ name, course, date }) {
+function App({ name, course, date, begin_date, end_date, hours_spent }) {
   return (
     <div className="App">
       <Icon />
-      <p className="byline">Certificate of completion</p>
+      <p className="byline">CERTIFICADO DE CONCLUSÃO</p>
 
       <div className="content">
-        <p>Awarded to</p>
-        <h1>{name}</h1>
-        <p>for completing:</p>
-        <h2>{course}</h2>
-      </div>
 
-      {date && (
-        <p className="date">
-          Issued on{' '}
-          <span className="bold">{date}</span>
-        </p>
-      )}
+        <div className='info'>
+          <p>Certificamos que <u>{name}</u> concluiu com êxito o curso de {course}, realizado
+          pelo projeto <b>Olá, Mundos!</b>, durante o período de {begin_date} a {end_date} com <b>carga
+          horária de {hours_spent} horas.</b></p>
+        </div>
+
+        <div className="data">
+          Campinas - SP, {date}
+        </div>
+
+        <div className="signature">
+          <div className="line">
+            ____________________________________________
+          </div>
+
+          <div className="image">
+            <img src={signature} width="120" height="300"></img>
+          </div>
+
+          <div className="role">
+            Caio Fernando 
+            <br></br> 
+            <b>Diretor de Ensino</b>
+          </div>
+        </div>
+
+      </div>
+      <img src={logo} className="logo" width="80" height="400"></img>
     </div>
   );
 }
 
 App.defaultProps = {
-  name: 'James Lee',
-  course: 'Creating PDFs with React & Make.cm',
-  date: 'March 15 2021'
+  name: 'Pedro Salvador',
+  course: 'Python',
+  date: '21 de Março de 2022',
+  begin_date: '01 de Setembro de 2021',
+  end_date: '31 de outubro de 2021',
+  hours_spent: '100'
 }
 
 const Icon = () => (
   <svg width="99" height="139" viewBox="0 0 99 139" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M0 0H99V138.406L52.1955 118.324L0 138.406V0Z" fill="white" />
-    <path d="M25.4912 83.2515C25.4912 79.4116 27.0222 75.7289 29.7474 73.0137C32.4727 70.2985 36.1689 68.7731 40.0229 68.7731C43.877 68.7731 47.5732 70.2985 50.2984 73.0137C53.0236 75.7289 54.5546 79.4116 54.5546 83.2515M40.0229 59.724C40.0229 55.8841 41.5539 52.2014 44.2791 49.4862C47.0044 46.7709 50.7006 45.2455 54.5546 45.2455C58.4087 45.2455 62.1049 46.7709 64.8301 49.4862C67.5553 52.2014 69.0863 55.8841 69.0863 59.724V83.2515" stroke="#0379FF" strokeWidth="10.6193" />
   </svg>
 )
 
